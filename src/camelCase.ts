9ea45c5d20bba1c9feb887513fe64666a35323c3
0pilatos0 <paulvanderlei@hotmail.nl>
2023-09-17 13:53:55 +0200

    feat(setup): added first util for setup testing

---
+++ src/camelCase.ts
@@ -0,0 +1,34 @@
+/**
+ * Converts a string to camelCase.
+ *
+ * @param {string} str - The input string to convert.
+ * @returns {string} The camelCase version of the input string.
+ */
+export default function camelCase(str: string): string {
+  /**
+   * Replaces non-letter characters with camelCase formatting.
+   *
+   * @param {string} word - The matched word or character.
+   * @param {number} index - The index of the matched word or character.
+   * @returns {string} The replacement for the matched word or character.
+   */
+  function camelCaseReplace(word: string, index: number): string {
+    if (index === 0) {
+      return word.toLowerCase();
+    }
+    if (!RegExp(/[a-zA-Z]/).exec(word)) {
+      return "";
+    }
+    return word.toUpperCase().replace(/[^a-zA-Z]/g, "");
+  }
+
+  if (
+    str.indexOf(" ") === -1 &&
+    str.indexOf("-") === -1 &&
+    str.indexOf("_") === -1
+  ) {
+    return str.toLowerCase();
+  }
+
+  return str.replace(/[\s_-]+(\w)/g, camelCaseReplace).replace(/\s+/g, "");
+}
---
+++ test/camelCase.test.ts
@@ -0,0 +1,32 @@
+import camelCase from "../src/camelCase";
+import { expect, describe, it } from "bun:test";
+
+describe("camelCase", () => {
+  it("should convert a string with spaces to camelCase", () => {
+    expect(camelCase("hello world")).toBe("helloWorld");
+  });
+
+  it("should convert a string with hyphens to camelCase", () => {
+    expect(camelCase("hello-world")).toBe("helloWorld");
+  });
+
+  it("should convert a string with underscores to camelCase", () => {
+    expect(camelCase("hello_world")).toBe("helloWorld");
+  });
+
+  it("should convert a string with mixed separators to camelCase", () => {
+    expect(camelCase("hello-world_world")).toBe("helloWorldWorld");
+  });
+
+  it("should handle an empty string", () => {
+    expect(camelCase("")).toBe("");
+  });
+
+  it("should handle a string with only one word", () => {
+    expect(camelCase("hello")).toBe("hello");
+  });
+
+  it("should handle a string with only one uppercase word", () => {
+    expect(camelCase("HELLO")).toBe("hello");
+  });
+});
