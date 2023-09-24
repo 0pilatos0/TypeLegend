66d0859bc8445da716af0af2fd8ecd517e202b2f
0pilatos0 <paulvanderlei@hotmail.nl>
2023-09-24 22:24:29 +0200

    added extra class import option

--- src/date/index.ts
+++ src/date/index.ts
@@ -1,1 +1,22 @@
+import { currentDate } from "./currentDate";
+import { currentDay } from "./currentDay";
+import { currentMonth } from "./currentMonth";
+import { currentYear } from "./currentYear";
+import { currentMonthName } from "./currentMonthName";
+import { currentDayName } from "./currentDayName";
+
+export class DateUtils {
+  static currentDate = currentDate;
+  static currentDay = currentDay;
+  static currentMonth = currentMonth;
+  static currentYear = currentYear;
+  static currentMonthName = currentMonthName;
+  static currentDayName = currentDayName;
+}
+
 export { currentDate } from "./currentDate";
+export { currentDay } from "./currentDay";
+export { currentMonth } from "./currentMonth";
+export { currentYear } from "./currentYear";
+export { currentMonthName } from "./currentMonthName";
+export { currentDayName } from "./currentDayName";
--- src/string/index.ts
+++ src/string/index.ts
@@ -1,2 +1,10 @@
+import { camelCase } from "./camelCase";
+import { capitalize } from "./capitalize";
+
+export class StringUtils {
+  static camelCase = camelCase;
+  static capitalize = capitalize;
+}
+
 export { camelCase } from "./camelCase";
 export { capitalize } from "./capitalize";
